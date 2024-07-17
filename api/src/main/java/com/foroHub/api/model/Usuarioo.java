ca9430da1f18836140188e23f183aa064fe78dbd
John Dimate <johnfelipe25@gmail.com>
2024-07-16 22:21:19 -0500

    Cambios en error usuario

--- api/src/main/java/com/foroHub/api/model/Usuarioo.java
+++ api/src/main/java/com/foroHub/api/model/Usuarioo.java
@@ -1,4 +1,21 @@
 package com.foroHub.api.model;
 
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
 public interface Usuarioo {
+    Collection<? extends GrantedAuthority> getAuthorities();
+
+    String getPassword();
+
+    String getUsername();
+
+    boolean isAccountNonExpired();
+
+    boolean isAccountNonLocked();
+
+    boolean isCredentialsNonExpired();
+
+    boolean isEnabled();
 }
